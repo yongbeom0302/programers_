3bf25d60684aacdc4b275df0037e3c064493eeca
yongbeom0302 <55076898+yongbeom0302@users.noreply.github.com>
2022-07-08 13:02:56 +0900

    Add files via upload
    홀짝구하기

---
+++ Main3.java
@@ -0,0 +1,21 @@
+package com.yong.test1;
+import java.util.Scanner;
+// 홀 짝 구하기
+public class Main3 {
+	public static void main (String[] args) {
+	
+		Scanner scan = new Scanner(System.in);
+		int a1 = scan.nextInt();
+		{
+		if(a1%2==0) {
+			System.out.println("even number");
+		}	
+		else{
+			System.out.println("odd number");
+		}
+		}
+	}	
+	}
+	
+	
+
