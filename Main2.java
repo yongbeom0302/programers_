83731945bf925c3c156a32e9c7d99a6ab5507adb
yongbeom0302 <55076898+yongbeom0302@users.noreply.github.com>
2022-07-08 13:02:28 +0900

    Add files via upload
    1~100 합구하기

---
+++ Main2.java
@@ -0,0 +1,18 @@
+package com.yong.test1;
+import java.util.Scanner;
+//1~100 까지의 합
+public class Main2 {
+	public static void main (String[] args) {
+	int answer =0;
+	for(int i=answer; i<101; i++) {
+		answer= answer + i;
+		
+		
+	}
+	System.out.println(answer);
+	}
+		
+	}
+	
+	
+
